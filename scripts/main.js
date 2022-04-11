f9bd1d91eba35ea888cd479786dc5ed2c9b0a9c8
Christopher Johnson <CLJohnson088@gmail.com>
2022-04-11 14:12:18 -0500

    Updated spacing

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Apply Dark Theme on page load based on user preference.
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        applyDarkTheme();
-    }
+  // Apply Dark Theme on page load based on user preference.
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    applyDarkTheme();
+  }
 
-    // Toggle Dark/Light Theme if the user preference changes.
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
-        const preferedTheme = event.matches ? 'dark' : 'light';
-        if (preferedTheme === 'dark') {
-            applyDarkTheme();
-        }
-        else {
-            applyLightTheme();
-        }
-    });
-    
-    // Toggle Dark/Light Theme.
-    const themeToggle = document.querySelector('#theme-toggle');
-    themeToggle.addEventListener('click', () => {
-        if (document.documentElement.classList.contains('dark-theme')) {
-            applyLightTheme();
-        }
-        else {
-            applyDarkTheme();
-        }
-    });
-    
-    function applyDarkTheme() {
-        document.documentElement.classList.add('dark-theme');
-        document.querySelector('#theme-toggle').textContent = 'brightness_high';
+  // Toggle Dark/Light Theme if the user preference changes.
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+    const preferedTheme = event.matches ? 'dark' : 'light';
+    if (preferedTheme === 'dark') {
+      applyDarkTheme();
     }
+    else {
+      applyLightTheme();
+    }
+  });
+    
+  // Toggle Dark/Light Theme.
+  const themeToggle = document.querySelector('#theme-toggle');
+  themeToggle.addEventListener('click', () => {
+    if (document.documentElement.classList.contains('dark-theme')) {
+      applyLightTheme();
+    }
+    else {
+      applyDarkTheme();
+    }
+  });
+    
+  function applyDarkTheme() {
+    document.documentElement.classList.add('dark-theme');
+    document.querySelector('#theme-toggle').textContent = 'brightness_high';
+  }
 
-    function applyLightTheme() {
-        document.documentElement.classList.remove('dark-theme');
-        document.querySelector('#theme-toggle').textContent = 'brightness_4';
-    }
+  function applyLightTheme() {
+    document.documentElement.classList.remove('dark-theme');
+    document.querySelector('#theme-toggle').textContent = 'brightness_4';
+  }
 });
